fbfe1370c8c04df0bb55fdb9f07431f74d3bcf7c
William <32031159+WRFleete@users.noreply.github.com>
2020-06-20 00:26:34 +1200

    Update PABXcontrol.ino
    adding comments

--- PABXcontrol.ino
+++ PABXcontrol.ino
@@ -63,11 +63,11 @@ on_hook_val = analogRead(I_loop_Vsense);
 void loop() {
  
  I_loopVal = analogRead(I_loop_Vsense);
-  if(!digitalRead(PSTN_Ring)){
+  if(!digitalRead(PSTN_Ring)){  //if the external phone line is ringing
     while(!digitalRead(PSTN_Ring)){
   digitalWrite(RingGen_EN,HIGH);
 //for(byte i = 0; i < 8; i++){
-  setHS(0);
+  setHS(0); //ring HS 0 on the intercom system
   digitalWrite(Status_LED,HIGH);}
   delay(100);
  // digitalWrite(Status_LED,LOW);
@@ -75,22 +75,26 @@ void loop() {
     delay(60);
  int test_off_hook = analogRead(I_loop_Vsense);
  delay(60);
- while(test_off_hook < (on_hook_val -120) || inHold){
+ while(test_off_hook < (on_hook_val -120) || inHold){ //wait for a phone on the intercom to go off hook
   digitalWrite(Status_LED,HIGH);
-  digitalWrite(RingGen_EN,LOW);
-  digitalWrite( PSTN_offhook,HIGH);
+  digitalWrite(RingGen_EN,LOW); //turn off the ring generator
+  digitalWrite( PSTN_offhook,HIGH); //take the POTS/PSTN line off hook
   //Serial.println(test_off_hook);
   delay(60);
-  if(readDTMF() == DTMF_Star){delay(60);if(inHold){inHold = 0;}else{inHold = 1;}}
+  if(readDTMF() == DTMF_Star){delay(60);if(inHold){inHold = 0;}else{inHold = 1;}} 
+  //decide if the "hold" relay should connect the intercom loop to the isolation transformer for the main phone line
+  //the DTMF star key should toggle the "hold" function. NB you should be able to inject audio into the phone while in hold
+  //hold nor dialing onto the main phone line will not work with pulse phones, works with DTMF only 
+  
           if(!inHold){digitalWrite( hold_Relay,HIGH); delay(60);
                  test_off_hook = analogRead(I_loop_Vsense);}else{checkTransferCall (); digitalWrite( hold_Relay,LOW);}
   
 
-  if(test_off_hook >= (on_hook_val - 120)){
+  if(test_off_hook >= (on_hook_val - 120)){ //when the intercom phones hang up
     test_off_hook = on_hook_val;
-  digitalWrite(PSTN_offhook,LOW);
-  digitalWrite( hold_Relay,LOW);
-  digitalWrite(Status_LED,LOW);
+  digitalWrite(PSTN_offhook,LOW); //hang up the main phone line
+  digitalWrite( hold_Relay,LOW); //disconnect the transformer from the intercom loop
+  digitalWrite(Status_LED,LOW); 
   }
   
   }
@@ -101,10 +105,10 @@ void loop() {
 
  
    
- if (I_loopVal < (on_hook_val - 30)){
+ if (I_loopVal < (on_hook_val - 30)){ //if main phone line wasnt ringing check if a handset is off hook
 
 
-if(onHookTimer >= onHookTimeout){
+if(onHookTimer >= onHookTimeout){ //stuff for resetting pulse count for pulse dialing
     pulTimeout = 50;
     countHookPulse = 0;
     onHookTimer = 0;
@@ -151,8 +155,8 @@ if(onHookTimer >= onHookTimeout){
  if(pulTimeout >= timeout){
   //pulseNumReady = true;
   if(countHookPulse >0 ){
-  DTMF_val = 10 - countHookPulse;
-  if(DTMF_val == 0){DTMF_val = 10;}
+  DTMF_val = 10 - countHookPulse; //process the pulse count as a "DTMF value"
+  if(DTMF_val == 0){DTMF_val = 10;} //zero dialed on pulse dial (10 pulses)
   countHookPulse = 0;
  
 }
@@ -168,23 +172,23 @@ if(onHookTimer >= onHookTimeout){
       digitalWrite(Status_LED,LOW);
       delay (30);
 
-         if(DTMF_val == 9){
+         if(DTMF_val == 9){ //dialing 9 enters a test mode to cycle the ringers on all phones on the intercom loop
           delay(60);
           testMode();
           
           }
 
         
-      if(DTMF_val <= 8){
-      setHS(DTMF_val - 1);
+      if(DTMF_val <= 8){ //dialing 1- 8 dials one of the 8 HS on the loop
+      setHS(DTMF_val - 1); // set the '138 to the HS to ring
      
       int off_hook = analogRead(I_loop_Vsense);
         delay (60);
-        while(off_hook >= (HS_off_hook - 40)){
+        while(off_hook >= (HS_off_hook - 40)){ //wait for a handset to pick up...cont...
          // int off_hook = analogRead(I_loop_Vsense);
           
           //Serial.println(test);
-          
+          //and pulse the ringer (can adjust for different ring pattern)
           digitalWrite(Status_LED,HIGH);
           digitalWrite(RingGen_EN,HIGH);
           delay(500);
@@ -201,10 +205,10 @@ if(onHookTimer >= onHookTimeout){
           
           int test = analogRead(I_loop_Vsense);
           delay (60);
-                   if(test >= (on_hook_val - 20)){
+                   if(test >= (on_hook_val - 20)){//...cont... or the caller hangs up
             off_hook = 0;
             }
-          while(test < (off_hook - 20)){
+          while(test < (off_hook - 20)){ //while in the call just wait for the handsets to hang up
             delay(60);
           test = analogRead(I_loop_Vsense);
           delay (60);
@@ -220,16 +224,18 @@ if(onHookTimer >= onHookTimeout){
      
         
         
-        if(DTMF_val == DTMF_0){
+        if(DTMF_val == DTMF_0){ //dialing 0 will take the main phoneline off hook
         digitalWrite(PSTN_offhook, HIGH);
         digitalWrite(Status_LED,HIGH);
-        digitalWrite( hold_Relay,HIGH);
+        digitalWrite( hold_Relay,HIGH); //route the main line to the intercom loop (via the isolation transformer)
         delay(60);
         int off_hook = analogRead(I_loop_Vsense);
         int test = analogRead(I_loop_Vsense);
         delay (250);
-        while(off_hook >= (test-70) || inHold){
-
+        while(off_hook >= (test-70) || inHold){ //if a handset is off hook or in hold keep the main line off hook
+//decide if the "hold" relay should connect the intercom loop to the isolation transformer for the main phone line
+  //the DTMF star key should toggle the "hold" function. NB you should be able to inject audio into the phone while in hold
+  //hold nor dialing onto the main phone line will not work with pulse phones, works with DTMF only 
           if(readDTMF() == DTMF_Star){delay(60);if(inHold){inHold = 0;}else{inHold = 1;}}
           if(!inHold){digitalWrite( hold_Relay,HIGH);delay(60);
                  test = analogRead(I_loop_Vsense);}else{
@@ -239,7 +245,7 @@ if(onHookTimer >= onHookTimeout){
           //Serial.println(test);
           delay(60);
           }     
-        digitalWrite(PSTN_offhook, LOW);
+        digitalWrite(PSTN_offhook, LOW); //when the intercom line phones hang up. hang up the main phone line (unless it was in hold)
         digitalWrite( hold_Relay,LOW);
         //digitalWrite(Status_LED,LOW);
         }
@@ -265,7 +271,7 @@ if(onHookTimer >= onHookTimeout){
     onHookTimer = 256;
     countHookPulse = 0;
     if(!messageSent){
-      Serial.println("On hook");
+      Serial.println("On hook"); //verbose diag info 
       messageSent = true;
       }
     //onHookTimer = 0;
@@ -298,7 +304,7 @@ delay(60);
   
 }
 
-void setHS(byte HSnum){
+void setHS(byte HSnum){ //sets the 74 series 138 output
   if(HSnum <=7){
   digitalWrite (HS_sel_B0,bitRead(HSnum,0));
 digitalWrite (HS_sel_B1,bitRead(HSnum,1));
@@ -307,7 +313,7 @@ digitalWrite (HS_sel_B2,bitRead(HSnum,2));
   
   }
 
-  byte readDTMF(){
+  byte readDTMF(){ //returns the DTMF value off the decoder chip
     byte DTMF_code;
    if(digitalRead(DTMF_B0)){bitSet(DTMF_code,0);}else{bitClear(DTMF_code,0);}
  if(digitalRead(DTMF_B1)){bitSet(DTMF_code,1);}else{bitClear(DTMF_code,1);}
@@ -316,7 +322,7 @@ digitalWrite (HS_sel_B2,bitRead(HSnum,2));
  return DTMF_code;
  }
  
- void testMode(){
+ void testMode(){ //ringer test for the intercom handsets
   Serial.println("ringer test");
   Serial.println("waiting for HS to hang up");
   while(analogRead(I_loop_Vsense) < on_hook_val - 10){
@@ -325,29 +331,31 @@ digitalWrite (HS_sel_B2,bitRead(HSnum,2));
     digitalWrite(Status_LED,LOW);
     
     }
-  while(analogRead(I_loop_Vsense) >= on_hook_val - 10){
-    for(byte i = 0; i < 8; i++){
+  while(analogRead(I_loop_Vsense) >= on_hook_val - 10){ //while all HS are on hook
+    for(byte i = 0; i < 8; i++){ //cycle through HS 1 - 8
       
       setHS(i);
       Serial.print("ringing HS ");
       Serial.println(i + 1);
       // for(byte j = 0; j < i+1; j++){
-      digitalWrite(RingGen_EN,HIGH);
+      digitalWrite(RingGen_EN,HIGH); //give the ringer circuit a kick
           delay(500);
           //digitalWrite(Status_LED,LOW);
-          digitalWrite(RingGen_EN,LOW);
+          digitalWrite(RingGen_EN,LOW); 
           delay(1000);//}
           
       
       }
-    Serial.println("pick up HS now to abort test");
+    Serial.println("pick up HS now to abort test"); //cycle until a HS is picked up
     delay(1000);
     }
   
   }
 
-  void checkTransferCall () {
-    
+  void checkTransferCall () { 
+   //when in hold using the main phone line (either from dialing out or if it was ringing)
+   //you can dial another HS on the loop to "transfer" a call
+     
     
     if(readDTMF() > 0){
                         byte DTMF_val = readDTMF();
